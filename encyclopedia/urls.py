b60ee2e14078202a00a5d054ea541582d25a0b05
Venish P <sec19ec070@sairamtap.edu.in>
2021-08-31 08:48:29 +0530

    changes done to some files

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,7 +6,7 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("wiki/", views.index, name="index"),
     path("wiki/<str:title>/", views.wiki_title, name="wiki_title"),
-    path("wiki/new_page/<str:title>/", views.wiki_title, name="new_page_title"),
-    path("wiki/new_page/",views.new_page, name="new_page"),
-    path("wiki/random_page/", views.random_page, name="RandomPage"),
+    path("wiki/new_page/<str:title>", views.wiki_title, name="new_page_title"),
+    path("wiki/new_page",views.new_page, name="new_page"),
+    path("wiki/random_page", views.random_page, name="RandomPage"),
 ]
--- entries/Portfolio.md
+++
@@ -1,4 +0,0 @@
-# Venish P
-### Sri Sai Ram Engineering College
-### [my github profile](https://github.com/Venish-P-sairam)
-### will be building my portfolio at this link [Venish Prabhu](https://Venish Prabhu.com)
